7a3f80aa65a8f62727f0f995b0f95965357f6997
Gabirondo-1C-2024 <valentina.gabirondo@ingenieria.uner.edu.ar>
2024-03-27 23:07:00 -0300

    Se agrega avance de proyecto 1, hasta el ejercicio 6

--- firmware/projects/Ejercicio4Guia1/main/Ejercicio4Guia1.c
+++ firmware/projects/Ejercicio4Guia1/main/Ejercicio4Guia1.c
@@ -49,28 +49,58 @@ void  convertToBcdArray (uint32_t data, uint8_t digits, uint8_t * bcd_number)
 	}
 }
 
-void cambioEstado(uint32_t data, gpioConf_t vectorGPIO){
+void cambioEstado(uint32_t data, gpioConf_t *vectorGPIO){
+	for(int i=0;i<4;i++){
+		if((data &(1<<i)) == 0){
+			GPIOOff(vectorGPIO[i].pin);}
+		else {GPIOOn(vectorGPIO[i].pin);
+		}
+	}
+}
 
+void prendeLEDS(uint32_t data, uint8_t digito, gpioConf_t *vectorGPIO, gpioConf_t *vectorGPIO_LCD){
 
+uint8_t arreglo [digito];
+convertToBcdArray (data, digito, arreglo);
+
+for(int i=0;1<digito;i++){
+	
+	cambioEstado(arreglo[i],vectorGPIO);
+
+	GPIOOn(vectorGPIO_LCD[i].pin);
+	GPIOOff(vectorGPIO_LCD[i].pin);
+
+}
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
 	
 uint32_t numero = 123;
-uint8_t arreglo [2];
-uint32_t digito = 3;
+int digito = 3;
+/*uint8_t arreglo [2];
 convertToBcdArray (numero, 3, arreglo);
-printf("El numero decimal: %ld",numero);
+printf("El numero decimal: %ld",numero);*/
 
 
 for(int j=0;j<digito;j++){
 	printf("%d",arreglo[j]);
 }
 
-gpioConf_t vectorBCD[4]={{GPIO_20,GPIO_OUTPUT},{GPIO_21,GPIO_OUTPUT},{GPIO_22,GPIO_OUTPUT},{GPIO_23,GPIO_OUTPUT}};
-for(int j=0;j<digito;j++){
-	cambioEstado(arreglo[j],vectorBCD);
+gpioConf_t vectorGPIO[4]={{GPIO_20,GPIO_OUTPUT},{GPIO_21,GPIO_OUTPUT},{GPIO_22,GPIO_OUTPUT},{GPIO_23,GPIO_OUTPUT}};
+GPIOInit(vectorGPIO[0].pin, vectorGPIO[0].dir);
+GPIOInit(vectorGPIO[1].pin, vectorGPIO[1].dir);
+GPIOInit(vectorGPIO[2].pin, vectorGPIO[2].dir);
+GPIOInit(vectorGPIO[3].pin, vectorGPIO[3].dir);
 
+gpioConf_t vectorGPIO_LCD[3]={{GPIO_19,GPIO_OUTPUT},{GPIO_18,GPIO_OUTPUT},{GPIO_9,GPIO_OUTPUT}};
+GPIOInit(vectorGPIO_LCD[0].pin, vectorGPIO_LCD[0].dir);
+GPIOInit(vectorGPIO_LCD[1].pin, vectorGPIO_LCD[1].dir);
+GPIOInit(vectorGPIO_LCD[2].pin, vectorGPIO_LCD[2].dir);
+
+
+
+for(int j=0;j<digito;j++){
+	cambioEstado(arreglo[j],vectorGPIO);
 }
 
 
